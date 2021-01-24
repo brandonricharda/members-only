4852958166ba843e2dbf9ab28c69f8505f1a6c11
brandonricharda <brandonricharda@gmail.com>
2021-01-24 01:49:18 -0500

    Corrects syntax errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user, only [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create]
 
     def new
     end
+
+    def index
+        @posts = Post.all
+    end
+
 end
