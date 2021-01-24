635cb22ff369ec815e01d6ee11c5c71203e7a3bb
brandonricharda <brandonricharda@gmail.com>
2021-01-24 01:44:28 -0500

    Creates Posts model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_063841) do
+ActiveRecord::Schema.define(version: 2021_01_24_064100) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
